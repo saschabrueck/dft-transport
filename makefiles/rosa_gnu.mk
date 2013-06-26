1d00a64a9608b930261259c227b486e88445a5ac
bruecks <bruecks@b4ce82db-76de-0410-b591-ab667595bc66>
2013-06-26 15:08:26 +0000

    * changed rosa makefile, needs relative path to extra libraries

--- makefiles/rosa_gnu.mk
+++ makefiles/rosa_gnu.mk
@@ -1,19 +1,10 @@
-#CPP = g++
-#CPP = mpicc
 CPP = CC
-CC = 
 
-CFLAGS = -g -Wall -DAdd_
+CFLAGS = -g -Wall -DAdd_ 
 CXXFLAGS = -std=c++11 $(CFLAGS)
 
-#--------------------------------------------------------------------------------------------
-# modify paths to the following libraries according to your local installations :
-# CP2K, ScaLapack, BLAS and LAPACK, libint, libxc (use the same libraries linked to CP2K)
-# UMFPACK, SuiteSparse_config, AMD, CHOLMOD, COLAMD
-#--------------------------------------------------------------------------------------------
-
-# Top library directory, relative to OMEN
-LIB_TOP       = /users/bruecks/interface
+# Directory of the libraries used by OMEN
+LIB_TOP       = /users/bruecks/GreenSolver/
 
 # Common include paths
 INCAZT        = -I$(LIB_TOP)/AZTEC/lib/
@@ -39,25 +30,9 @@ QHULLLIB      = $(LIB_TOP)/QHULL/src/libqhull.a
 SUPERLULIB    = $(LIB_TOP)/SuperLU_DIST_2.0/Lib/libsuperlu_dist_2.0.a
 UMFPACKLIB    = $(LIB_TOP)/UMFPACK/Lib/libumfpack.a
 
-LFLAGS = -L/users/bruecks/cp2k/lib/CRAY-XE6-gfortran-hwtopo/popt/ #\
-	-L/opt/xt-libsci/default/gnu/47/interlagos/lib #\
-         -L/users/bruecks/GreenSolver/UMFPACK/Lib/ #\
-	-L/data/seyedb/scalapack/install/lib/ #\
-	-L/data/vjoost/libint_ham/install/lib/ #\
-	-L/data/vjoost/libxc-1.2.0/install/lib/ #\
-	-L/data/seyedb/SuiteSparse_config/lib #\
-	-L/opt/intel/composerxe-2011.3.174/mkl/lib/intel64
+LFLAGS = -L/users/bruecks/cp2k/lib/CRAY-XE6-gfortran-hwtopo/popt/ 
 DFLAGS =
-LIBS = $(SUPERLULIB) -lcp2k_lib -lcp2k_base_lib -lcp2k_dbcsr_lib -lcp2k_fft_lib -lcp2k_ma_lib -lcp2k_elpa_lib \
-	-lm -lgfortran -lstdc++\
-	-lfftw3 -lsci_gnu #\
-        -lumfpack #\
-	-lscalapack -lreflapack -lrefblas #\
-        -lderiv -lint -lxc #\
-	-lumfpack -lamd -lcholmod -lcolamd -lrt -lsuitesparseconfig #\
-	-lmkl_gf_lp64 -lmkl_scalapack_lp64 -lmkl_sequential -lmkl_core -lmkl_blacs_openmpi_lp64 
-#INCLUDES = -I/data/seyedb/SuiteSparse_config/include 
-#INCLUDES = -I/users/bruecks/GreenSolver/UMFPACK/Include/ \
-	   -I/users/bruecks/GreenSolver/UFconfig/
-#INCLUDES = -I/opt/xt-libsci/default/gnu/47/interlagos/include/
-INCLUDES = $(INCSLU) $(INCUMF)
+LIBS = $(SUPERLULIB) $(UMFPACKLIB) $(AMDLIB) $(METISLIB) $(MUMPSLIB) $(PORDLIB) \
+	-lcp2k_lib -lcp2k_base_lib -lcp2k_dbcsr_lib -lcp2k_fft_lib -lcp2k_ma_lib -lcp2k_elpa_lib \
+	-lm -lgfortran -lstdc++	-lfftw3 -lsci_gnu 
+INCLUDES = $(INCSLU) $(INCUFC) $(INCUMF) $(INCMPS) $(INCPOR) $(INCAMD)
