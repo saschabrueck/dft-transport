18154e8095a7e05cc224cb2554e5f005e727b8fc
mauro <mauro@b4ce82db-76de-0410-b591-ab667595bc66>
2013-06-28 11:25:59 +0000

    * Fixing a few things Makefile.in ** Fixed rm -rf *o to be rm -rf *.o in clean target ** Added a target to allow for a different main routine (in mymain.cc) for isolated code testing ** Treat .C and .cpp files equally (build object files for both) * Adjusted ignores on .src accordingly * Added a arch file for attelas.iis.ee

--- makefiles/iis.ee-attelas.mk
+++ makefiles/iis.ee-attelas.mk
@@ -1,4 +1,7 @@
 # assumes you use the stuff under /home/mauro/sw
+#
+# TODO: ensure that there's no problem for pardiso when linking against a newer
+# limf.so than the one it was compiled with (Mathieu's intel)
 
 MBASE = /home/mauro/sw
 
@@ -6,7 +9,9 @@ CPP = mpic++
 CC =
 
 CFLAGS = -g -Wall -DAdd_
-CXXFLAGS = -std=c++11 $(CFLAGS) -DMMC
+CXXFLAGS = -std=c++11 -fopenmp $(CFLAGS) -DMMC
+PARDISO = /home/mauro/omendft/prototypes/transport/pardiso/Pardiso/libpardiso490-INTEL120-X86-64.so
+PARDISO_INTEL_LIB = -L/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t/ -L/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/lib/ -lifcore -lifport -lpthread -limf -lsvml -lintlc -lm -lpthread -lm -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/lib/ -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t
 
 CP2KLIB = /home/mauro/sw/cp2k/cp2k/lib/iis_ee-x86-64-gcc-openblas-scalapack/popt
 
@@ -22,4 +27,5 @@ LIBS = -lcp2k_lib -lcp2k_base_lib -lcp2k_dbcsr_lib -lcp2k_fft_lib -lcp2k_ma_lib 
 	 		-lcholmod -lcolamd -lrt -lsuitesparseconfig \
 			-lcamd -lccolamd -lmetis \
 	 -lopenblas \
-	 -lscalapack
+	 -lscalapack \
+	 -limf -lsvml -liomp5 -lintlc $(PARDISO_INTEL_LIB)
