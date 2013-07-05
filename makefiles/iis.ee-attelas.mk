69e568eaeae398cf43d7c14b532cfa64b4b521ac
Mauro Calderara <MauroCalderara@users.noreply.github.com>
2013-07-05 11:22:09 +0000

    * new makefiles that reflect the new paths under /home/mauro/sw (for those of you using this). Note: the easiest way to set up the environment for using the stuff there is to 'source ~mauro/sw/set_environment-gcc-scalapack.sh'

--- makefiles/iis.ee-attelas.mk
+++ makefiles/iis.ee-attelas.mk
@@ -10,23 +10,23 @@ CC =
 
 CFLAGS = -g -Wall -DAdd_
 CXXFLAGS = -std=c++11 -fopenmp $(CFLAGS) -DMMC
-PARDISO_SO = /home/mauro/omendft/prototypes/transport/pardiso/Pardiso/libpardiso490-INTEL120-X86-64.so
+PARDISO_SO = /home/mauro/sw/pardiso/lib/libpardiso490-INTEL120-X86-64.so
 PARDISO_INTEL_LIB = -L/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t/ -L/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/lib/ -lifcore -lifport -lpthread -limf -lsvml -lintlc -lm -lpthread -lm -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/lib/ -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t
 
-CP2KLIB = /home/mauro/sw/cp2k/cp2k/lib/iis_ee-x86-64-gcc-openblas-scalapack/popt
+CP2KLIB = /home/mauro/sw/cp2k/cp2k-transport/lib/iis_ee-x86-64-gcc-openblas-scalapack/popt
 
 # Use scalapack, suitesparse/umfpack is available on this system.
-LFLAGS = -L$(CP2KLIB) -L$(MBASE)/gcc/4.8.1/lib64 -L$(MBASE)/fftw/gcc/lib -L$(MBASE)/suitesparse/lib -L$(MBASE)/openblas/lib -L$(MBASE)/scalapack/lib
+LFLAGS = -L$(CP2KLIB) -L$(MBASE)/gcc/4.8.1/lib64 -L$(MBASE)/fftw/gcc-attelas/lib -L$(MBASE)/suitesparse/gcc-attelas/lib -L$(MBASE)/openblas/gcc-attelas/lib -L$(MBASE)/scalapack/gcc-attelas/lib
 
 DFLAGS =
 
 LIBS = -lcp2k_lib -lcp2k_base_lib -lcp2k_dbcsr_lib -lcp2k_fft_lib -lcp2k_ma_lib -lcp2k_elpa_lib \
    -lm -lgfortran -lstdc++ \
    -lfftw3 \
-   -I$(MBASE)/suitesparse/include -lumfpack -lamd \
+   -I$(MBASE)/suitesparse/gcc-attelas/include -lumfpack -lamd \
 	 		-lcholmod -lcolamd -lrt -lsuitesparseconfig \
 			-lcamd -lccolamd -lmetis \
 	 -lopenblas \
-	 -lscalapack \
+	 -lscalapack
 
-PARDISO_LIBS = $(PARDISO_INTEL_LIB) -limf -lsvml -liomp5 -lintlc
+PARDISO_LIBS = $(PARDISO_INTEL_LIB) $(PARDISO_SO) -limf -lsvml -liomp5 -lintlc
