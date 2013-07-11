da46055ba1467dfcaeda8f5401c108d9c5a4119d
mauro <mauro@b4ce82db-76de-0410-b591-ab667595bc66>
2013-07-11 15:23:33 +0000

    * Changes to make our code compile with intel icpc 2011 (however, it segfaults somewhere before the transport calculation): ** Removed or #ifdef'ed some C++11 stuff in my code ** Adjusted construction of Mathematica precalculation script ** Adjusted the arch-makefile so that the code compiles on attelas

--- makefiles/iis.ee-attelas-intel2011.mk
+++ makefiles/iis.ee-attelas-intel2011.mk
@@ -8,14 +8,22 @@
 # Or perform the required steps manually for your csh.
 
 MBASE = /home/mauro/sw
+IBASE = /usr/pack/intel_compiler-11.1.075-af
 
 CPP = mpic++
 CC =
 
-CFLAGS = -g -Wall -DAdd_ -wd981			# -wd981 is recommended for c++ even by intel
+#CFLAGS = -g -Wall -DAdd_ -wd981			# -wd981 is recommended for c++ even by intel
+CFLAGS = -g -DAdd_ -wd981 # -wd981 is recommended for c++ even by intel
 CXXFLAGS = -std=c++0x -openmp $(CFLAGS)
 PARDISO_SO = /home/mauro/sw/pardiso/lib/libpardiso490-INTEL120-X86-64.so
-INTEL_LIB = -L/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t -lmkl_intel_lp64 -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 -lmkl_sequential -lmkl_core -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t/ -L/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/lib/ -lifcore -lifport -lpthread -limf -lsvml -lintlc -lm -lpthread -lm -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/lib/ -Wl,-rpath,/usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t
+INTEL_LIB = -L$(IBASE)/Linux-x86_64/mkl/lib/em64t \
+							-lmkl_intel_lp64 -lmkl_scalapack_lp64 -lmkl_blacs_openmpi_lp64 \
+							-lmkl_sequential -lmkl_core \
+							-Wl,-rpath,$(IBASE)/Linux-x86_64/mkl/lib/em64t \
+						-L$(IBASE)/Linux-x86_64/lib -lifcore -lifport -lpthread -limf \
+							-lsvml -lintlc -lm -lpthread -lm \
+							-Wl,-rpath,$(IBASE)/Linux-x86_64/lib
 
 CP2KLIB = /home/mauro/sw/cp2k/cp2k-transport/lib/iis_ee-x86-64-oldintel/popt
 
@@ -31,6 +39,6 @@ LIBS = -lcp2k_lib -lcp2k_base_lib -lcp2k_dbcsr_lib -lcp2k_fft_lib -lcp2k_ma_lib 
    -I$(MBASE)/suitesparse/intel-2011/include -lumfpack -lamd \
 	 		-lcholmod -lcolamd -lrt -lsuitesparseconfig \
 			-lcamd -lccolamd -lmetis \
-	 -lmpi_f77
+	 -lmpi_f77 -Wl,-rpath,$(MBASE)/openmpi/intel-2011/lib
 
 PARDISO_LIBS = $(PARDISO_INTEL_LIB) $(PARDISO_SO) -limf -lsvml -liomp5 -lintlc
