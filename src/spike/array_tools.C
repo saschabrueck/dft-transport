2c5fdfa45fd979a6ea43c33c7bfa24e4af1578f9
Mauro Calderara <MauroCalderara@users.noreply.github.com>
2014-02-27 18:36:14 +0000

    * cleaning up, will commit once rest is merged

--- src/spike/array_tools.C
+++
@@ -1,8 +0,0 @@
-#include "array_tools.H"
-
-void full_conjugate_transpose(int n1, int n2, CPX *in, CPX* out)
-{
-    for (int j=0;j<n2;j++)
-        for (int i=0;i<n1;i++)
-            out[j*n1+i]=conj(in[i*n2+j]);
-}
