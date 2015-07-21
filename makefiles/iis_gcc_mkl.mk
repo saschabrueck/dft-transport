687b650818aa92091c95aaabc1f84bc8dda96f9c
bruecks <bruecks@b4ce82db-76de-0410-b591-ab667595bc66>
2015-07-21 18:16:21 +0000

    makefile for local cluster

--- makefiles/iis_gcc_mkl.mk
+++ makefiles/iis_gcc_mkl.mk
@@ -3,7 +3,9 @@ YACC          = bison
 
 GFORTL       = /home/nanotcad/sw/gcc/4.8.2/dahu/lib64
 
-LIB_TOP       = /usr/ela/home/bruecks/GreenSolver_Intel
+LIB_TOP      = /home/bruecks/GreenSolver_Intel
+
+LIB_SW       = /home/nanotcad/sw
 
 # MKL library paths
 MKLLIB       = /usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/mkl/lib/em64t
@@ -14,25 +16,27 @@ INTLIB       = /usr/pack/intel_compiler-11.1.075-af/Linux-x86_64/lib
 INCAZT        = -I$(LIB_TOP)/AZTEC/lib/
 INCQHU        = -I$(LIB_TOP)/QHULL/src/
 INCAMD        = -I$(LIB_TOP)/AMD/Include/
-INCSLU        = -I$(LIB_TOP)/SuperLU_DIST_2.0/SRC/
-INCMPS        = -I$(LIB_TOP)/MUMPS_4.10.0/include/
 INCPDI        = -I$(LIB_TOP)/PDIV/
-INCPOR        = -I$(LIB_TOP)/MUMPS_4.10.0/PORD/include/
 INCUFC        = -I$(LIB_TOP)/UFconfig/
 INCUMF        = -I$(LIB_TOP)/UMFPACK/Include
+INCSLU        = -I$(LIB_SW)/SuperLU_DIST_3.3/SRC/
+INCPEX        = -I$(LIB_SW)/pexsi_v0.8.0/include/
+INCMPS        = -I$(LIB_SW)/MUMPS_5.0.0/include/
+INCPOR        = -I$(LIB_SW)/MUMPS_5.0.0/PORD/include/
 
 # Common library paths
 ARPACKLIB     = $(LIB_TOP)/ARPACK2/libarpack.a
 AMDLIB        = $(LIB_TOP)/AMD/Lib/libamd.a
 AZTECLIB      = $(LIB_TOP)/AZTEC/lib/libaztec.a
-METISLIB      = $(LIB_TOP)/Metis/libmetis.a
-MUMPSLIB      = $(LIB_TOP)/MUMPS_4.10.0/lib/libzmumps.a
-MUMPDLIB      = $(LIB_TOP)/MUMPS_4.10.0/lib/libdmumps.a
-MUMPSCOM      = $(LIB_TOP)/MUMPS_4.10.0/lib/libmumps_common.a
 PDIVLIB       = $(LIB_TOP)/PDIV/libpdiv.a
-PORDLIB       = $(LIB_TOP)/MUMPS_4.10.0/lib/libpord.a
 QHULLLIB      = $(LIB_TOP)/QHULL/src/libqhull.a
 UMFPACKLIB    = $(LIB_TOP)/UMFPACK/Lib/libumfpack.a
+SLULIB        = $(LIB_SW)/SuperLU_DIST_3.3/lib/libsuperlu_dist_3.3.a
+PEXLIB        = $(LIB_SW)/pexsi_v0.8.0/src/libpexsi_dahu.a
+PARMLIB       = $(LIB_SW)/parmetis/lib/libparmetis.a $(LIB_SW)/parmetis/lib/libmetis.a
+MUMPSLIB      = $(LIB_SW)/MUMPS_5.0.0/lib/libzmumpsdahu.a
+MUMPSCOM      = $(LIB_SW)/MUMPS_5.0.0/lib/libmumps_commondahu.a
+PORDLIB       = $(LIB_SW)/MUMPS_5.0.0/PORD/lib/libpord.a
 
 # CP2K library path
 CP2KLIB = /home/nanotcad/sw/cp2k/lib/iis_gcc_mkl/popt
@@ -47,14 +51,13 @@ DMALLOC = -L/usr/local/allinea/tools/lib/64/  -ldmallocthcxx -z muldefs
 
 LFLAGS = -L$(CP2KLIB) -L$(MKLLIB) -L$(INTLIB)
 
-DFLAGS = -DAdd_ -DMPICH_IGNORE_CXX_SEEK -DHAVE_UMFPACK -Dlibcp2k
+DFLAGS = -DAdd_ -DMPICH_IGNORE_CXX_SEEK -DHAVE_MUMPS -Dlibcp2k
 
-LINLIN = /home/bruecks/CSelInv/EXAMPLES/C2Finterface.o /home/bruecks/CSelInv/LIB/libcsupldlt.a
-
-INCLUDES = -I$(MKLINC) $(INCSLU) $(INCUFC) $(INCUMF) $(INCMPS) $(INCPOR) $(INCAMD) $(INCAZT) $(INCQHU)
+INCLUDES = -I$(MKLINC) $(INCSLU) $(INCUFC) $(INCUMF) $(INCMPS) $(INCPOR) $(INCAMD) $(INCAZT) $(INCQHU) $(INCPEX)
 
 LIBS = -lcp2k \
 	$(UMFPACKLIB) $(AMDLIB) $(MUMPSLIB) $(MUMPDLIB) $(MUMPSCOM) $(PORDLIB) $(METISLIB) $(AZTECLIB) $(QHULLLIB) $(ARPACKLIB) \
+	$(PEXLIB) $(SLULIB) $(PARMLIB) \
         -Wl,-rpath,$(MKLLIB) -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lmkl_blacs_intelmpi_lp64 \
 	-Wl,-rpath,$(INTLIB) -lifcore -lifport -limf -lsvml -lintlc -liomp5 -lpthread \
 	-lm -lrt -fopenmp -Wl,-rpath,$(GFORTL) -L$(GFORTL) -lgfortran -lstdc++
