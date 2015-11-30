cfe74e529fdd2e8978c8568ef75f4a56fef3ea93
seyedb <seyedb@b4ce82db-76de-0410-b591-ab667595bc66>
2015-11-30 15:23:14 +0000

    cleanup input; pass energy_diff; add matrix cutout; update arch1.mk.

--- makefiles/arch1.mk
+++ makefiles/arch1.mk
@@ -7,55 +7,40 @@ GCC      = gcc
 CFLAGS   = -g -w -Wall -fopenmp 
 CXXFLAGS = -std=c++11 $(CFLAGS)
 
-TOP_DIR        = /data/seyedb
-TOOLCHAIN_DIR = /data/vjoost/toolchain-gcc492/install
+TOP_DIR       = /data/seyedb/cp2k-omen
+TOOLCHAIN_DIR = /data/vjoost/toolchain-r16358/install
+LIB_TOP       = $(TOP_DIR)/libs
 
 # Common include paths
-IAZTEC       = $(TOP_DIR)/Aztec2.1.1.0/lib/
-IPORD        = $(TOP_DIR)/MUMPS_4.10.0/PORD/include/
-IMUMPS       = $(TOP_DIR)/MUMPS_4.10.0/include/
-ISPARSE      = $(TOP_DIR)/SuiteSparse_config/include/
-ISUITESPARSE = $(TOP_DIR)/SuiteSparse_config/
-IQHULL       = $(TOP_DIR)/qhull-2012.1/src/libqhull/
-ISLU         = $(TOP_DIR)/SuperLU_DIST_3.3/SRC
+INCMUMPS      = $(LIB_TOP)/mumps/include/
+INCAZTEC      = $(LIB_TOP)/aztec/include/
+INCQHULL      = $(LIB_TOP)/qhull/include/
+INCSSPARSE    = $(LIB_TOP)/suitesparse/include/
+INCPEXSI      = $(LIB_TOP)/pexsi/include/
+INCSLUDIST    = $(TOOLCHAIN_DIR)/include/superlu_dist_3.3/
 
 # Common library paths
-LAZTEC       = $(TOP_DIR)/Aztec2.1.1.0/lib
-LSLUD        = $(TOP_DIR)/SuperLU_DIST_3.3/lib
-LMETIS       = $(TOP_DIR)/parmetis-4.0.2/build/Linux-x86_64/libmetis 
-LPARMETIS    = $(TOP_DIR)/parmetis-4.0.2/build/Linux-x86_64/libparmetis
-LESMUMPS     = $(TOP_DIR)/scotch_6.0.0_esmumps/lib
-LMUMPS       = $(TOP_DIR)/MUMPS_4.10.0/lib 
-LSPARSE      = $(TOP_DIR)/SuiteSparse_config/lib
-LSUITESPARSE = $(TOP_DIR)/SuiteSparse_config 
-LQHULL       = $(TOP_DIR)/qhull-2012.1/lib 
-LARPACK      = $(TOP_DIR)/ARPACK
-LIBPEXSI     = $(TOP_DIR)/pexsi_v0.5.5/src
-#LIBPEXSI     = $(TOP_DIR)/pexsi/src
-LIBINT       = $(TOOLCHAIN_DIR)/lib/ 
-LIBXC        = $(TOOLCHAIN_DIR)/lib/
-SCALAPACK    = $(TOOLCHAIN_DIR)/lib/ 
+LIBARPACK     = $(LIB_TOP)/arpack/lib
+LIBMUMPS      = $(LIB_TOP)/mumps/lib
+LIBAZTEC      = $(LIB_TOP)/aztec/lib
+LIBQHULL      = $(LIB_TOP)/qhull/lib
+LIBSSPARSE    = $(LIB_TOP)/suitesparse/lib
+LIBCP2K       = $(TOP_DIR)/cp2k/cp2k/lib/local_omencp2k/popt
+TOOLCHAIN_LIB = $(TOOLCHAIN_DIR)/lib
 
-# CP2K library path
-CP2KLIB  = $(TOP_DIR)/cp2k-omen/cp2k/cp2k/lib/local/pdbg/
+LFLAGS   = -L$(LIBCP2K) -L$(TOOLCHAIN_LIB) -L$(LIBAZTEC) \
+           -L$(LIBMUMPS) -L$(LIBSSPARSE) -L$(LIBQHULL) -L$(LIBARPACK)
 
-LFLAGS   = -L$(CP2KLIB) -L$(SCALAPACK) -L$(LAZTEC) -L$(LMETIS) -L$(LPARMETIS) -L$(LSLUD) -L$(LESMUMPS) \
-           -L$(LMUMPS) -L$(LSPARSE) -L$(LSUITESPARSE) -L$(LQHULL) -L$(LARPACK) \
-           -L$(LIBINT) -L$(LIBXC) -L$(LIBPEXSI)
-
-DFLAGS   = -Dlibcp2k -DAdd_ 
-
-PARDISO_SO = -L$(TOP_DIR)/pardiso/lib -lpardiso500-MPI-GNU472-X86-64 
+DFLAGS   = -Dlibcp2k -DAdd_
 
 LIBS     = -lm -lgfortran -lstdc++ \
            -lcp2k \
-           -lfftw3 -laztec -lpexsi \
-           -lzmumps -ldmumps -lmumps_common -lpord \
+           -lfftw3 -laztec -lpexsi_linux_v0.9.0 \
            -lumfpack -lamd -lccolamd -lcholmod -lcolamd -lcamd -lccolamd \
-           -lparmetis -lmetis -lesmumps -lsuperlu_dist_3.3 -lscotch -lscotcherr \
+           -lparmetis -lmetis -lzmumps -ldmumps -lmumps_common -lpord -lsuperlu_dist_3.3 \
            -lscalapack -lreflapack -lrefblas \
            -lsuitesparseconfig \
-           -lqhullstatic -larpack -lderiv -lint -lxc -lmpifort
-       
-INCLUDES = -I$(IAZTEC) -I$(IPORD) -I$(IMUMPS) -I$(ISPARSE) -I$(ISUITESPARSE) -I$(IQHULL) -I$(ISLU)
+           -lqhullstatic -larpack -lderiv -lint -lxcf90 -lxc -lmpifort
+
+INCLUDES = -I$(INCAZTEC) -I$(INCSLUDIST) -I$(INCSSPARSE) -I$(INCMUMPS) -I$(INCQHULL) -I$(INCPEXSI)
 
