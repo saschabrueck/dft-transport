58dc9d03037822b50416af3d2bef1c501f7b082c
bruecks <32339451+saschabrueck@users.noreply.github.com>
2016-09-22 20:50:35 +0000

    new main file which requires x.inp and writes to stdout

--- src/main.cpp
+++ src/main.cpp
@@ -31,52 +31,11 @@ Poisson *OMEN_Poisson_Solver;
 
 int main (int argc, char **argv)
 {
-   char *input_file;
-   char *output_file;
-   char *command_file;
-#ifdef libcp2k
-   force_env_t force_env;
-   int natom, n_el, n_el_pos, n_el_force;
-   double e_pot;
-   double *pos, *force;
-#endif
-
-   std::cout.precision(12);
-
-   std::string inpfile_path = argv[1]+std::string(".inp");
-   input_file=new char[inpfile_path.size()+1];
-   input_file[inpfile_path.size()]=0;
-   memcpy(input_file,inpfile_path.c_str(),inpfile_path.size());
-   std::string outfile_path = argv[1]+std::string(".out");
-   output_file=new char[outfile_path.size()+1];
-   output_file[outfile_path.size()]=0;
-   memcpy(output_file,outfile_path.c_str(),outfile_path.size());
-   std::string commandfile_path = argv[1]+std::string(".cmd");
-   command_file=new char[commandfile_path.size()+1];
-   command_file[commandfile_path.size()]=0;
-   memcpy(command_file,commandfile_path.c_str(),commandfile_path.size());
-
    MPI_Init(&argc,&argv);
 
-#ifdef libcp2k
-   cp2k_init_without_mpi();
-   cp2k_create_force_env_comm(&force_env, input_file, output_file, MPI_Comm_c2f(MPI_COMM_WORLD));
-   cp2k_transport_set_callback(force_env, &c_scf_method);
-   cp2k_get_natom(force_env, &natom);
-
-   n_el = natom*3;
-   n_el_pos = natom*3;
-   n_el_force = natom*3;
-   pos = new double[n_el_pos];
-   force = new double[n_el_force];
-   std::fill_n(pos, n_el_pos, 0);
-   std::fill_n(force, n_el_force, 0);
-#endif
-
 #ifdef HAVE_OMEN_POISSON
-   int do_omen_poisson=0;
-   if ((yyin = fopen(command_file,"r")) != NULL) do_omen_poisson=1;
-   if (do_omen_poisson) {
+   if (argc>2) {
+      yyin = fopen(argv[2],"r");
       init_parameters();
       yyrestart(yyin);
       yyparse();
@@ -97,30 +56,31 @@ int main (int argc, char **argv)
 #endif
 
 #ifdef libcp2k
+   cp2k_init_without_mpi();
+   force_env_t force_env;
+   cp2k_create_force_env_comm(&force_env, argv[1], "__STD_OUT__", MPI_Comm_c2f(MPI_COMM_WORLD));
+   cp2k_transport_set_callback(force_env, &c_scf_method);
+   int natom;
+   cp2k_get_natom(force_env, &natom);
    cp2k_calc_energy_force(force_env);
+   double e_pot;
    cp2k_get_potential_energy(force_env, &e_pot);
-   cp2k_get_forces(force_env, force, n_el_force);
-   cp2k_get_positions(force_env, pos, n_el);
+   std::vector<double> force(natom*3,0.0);
+   cp2k_get_forces(force_env, &force[0], force.size());
+   std::vector<double> pos(natom*3,0.0);
+   cp2k_get_positions(force_env, &pos[0], pos.size());
    cp2k_destroy_force_env(force_env);
    cp2k_finalize_without_mpi();
 #endif
 
 #ifdef HAVE_OMEN_POISSON
-   if (do_omen_poisson) {
+   if (argc>2) {
       delete OMEN_Poisson_Solver;
       delete FEM;
       delete Wire;
       delete_parameters();
    }
 #endif
-
-#ifdef libcp2k   
-   delete [] pos;
-   delete [] force;
-#endif
-   delete [] input_file;
-   delete [] output_file;
-   delete [] command_file;
 
    MPI_Finalize();
    return 0;
