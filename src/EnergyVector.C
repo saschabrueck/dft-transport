66c84dee46d65014cefaabee401b89944713a6cf
Mohammad Hossein Bani-Hashemian <seyedb@users.noreply.github.com>
2017-09-01 15:31:56 +0000

    be compatible with the latest versions of libraries; add some headers.

--- src/EnergyVector.C
+++ src/EnergyVector.C
@@ -17,6 +17,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include "EnergyVector.H"
 #include <iterator>
 #include <limits>
+#include <vector>
 
 Energyvector::Energyvector()
 {
