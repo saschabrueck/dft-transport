efefd4504ab092c0150842ea90fbbfb6a3843326
seyedb <seyedb@b4ce82db-76de-0410-b591-ab667595bc66>
2017-09-01 14:14:39 +0000

    add mk files.

---
+++ makefiles/mont-fort1.mk
@@ -0,0 +1,50 @@
+# source /home/seyedb/cp2k-omen/cp2k/cp2k/tools/toolchain/install/setup
+#
+LEX      = flex
+YACC     = bison
+
+CPP      = mpic++ 
+GCC      = gcc 
+NVCC     = 
+
+CFLAGS    = -g -w -Wall -fopenmp 
+CXXFLAGS  = -std=c++11 $(CFLAGS)
+NVCCFLAGS = 
+
+TOP_DIR       = /home/seyedb/cp2k-omen
+LIB_TOP       = $(TOP_DIR)/libs
+TOOLCHAIN     = $(TOP_DIR)/cp2k/cp2k/tools/toolchain
+
+# Common include paths
+INCMUMPS      = $(LIB_TOP)/MUMPS/include/
+INCAZTEC      = $(LIB_TOP)/Aztec/include/
+INCHYPRE      = $(LIB_TOP)/hypre/include/
+INCQHULL      = $(LIB_TOP)/qhull/include/libqhull/
+INCSSPARSE    = $(LIB_TOP)/SuiteSparse/include/
+INCPEXSI      = $(TOOLCHAIN)/install/pexsi-0.10.1/include/
+INCSLUDIST    = $(TOOLCHAIN)/install/superlu_dist-5.1.2/include/
+
+# Common library paths
+LIBPARMETIS   = $(TOOLCHAIN)/install/parmetis-4.0.3/lib
+LIBMUMPS      = $(LIB_TOP)/MUMPS/lib
+LIBAZTEC      = $(LIB_TOP)/Aztec/lib
+LIBHYPRE      = $(LIB_TOP)/hypre/lib
+LIBQHULL      = $(LIB_TOP)/qhull/lib
+LIBSSPARSE    = $(LIB_TOP)/SuiteSparse/static
+LIBSLUDIST    = $(TOOLCHAIN)/install/superlu_dist-5.1.2/lib
+LIBPEXSI      = $(TOOLCHAIN)/install/pexsi-0.10.1/lib
+LIBCP2K       = $(TOP_DIR)/cp2k/cp2k/lib/local/popt
+
+LFLAGS   = -L$(LIBCP2K) -L$(LIBSSPARSE) -L$(LIBAZTEC) -L$(LIBPEXSI) -L$(LIBPARMETIS) \
+           -L$(LIBMUMPS) -L$(LIBSSPARSE) -L$(LIBSLUDIST) -L$(LIBAZTEC) -L$(LIBHYPRE) -L$(LIBQHULL)
+
+DFLAGS   = -DAdd_ 
+
+LIBS     = -lrt -ldl -lstdc++ -lcp2k -lfftw3 -laztec -lpexsi \
+	-lumfpack -lamd -lcholmod -lcolamd -lccolamd -lcamd -lsuitesparseconfig \
+	-lparmetis -lmetis -lzmumps -ldmumps -lmumps_common -lpord -lsuperlu_dist \
+	-lHYPRE -lscalapack -llapack -lopenblas -lqhullstatic \
+	-lxsmmf -lxsmm -lderiv -lint -lxcf90 -lxc -lgfortran -lmpifort
+
+INCLUDES = -I$(INCSLUDIST) -I$(INCAZTEC) -I$(INCHYPRE) -I$(INCSSPARSE) -I$(INCMUMPS) -I$(INCQHULL) -I$(INCPEXSI)
+
